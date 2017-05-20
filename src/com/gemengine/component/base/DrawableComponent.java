e2a5c5b405323d14748c792e3c603ba9bef10f6b
ughuuu <daian.dragos@yahoo.com>
2017-05-20 10:33:47 +0300

    take drawable out forn ow

--- src/com/gemengine/component/base/DrawableComponent.java
+++
@@ -1,9 +0,0 @@
-package com.gemengine.component.base;
-
-import com.gemengine.system.ComponentSystem;
-
-public abstract class DrawableComponent extends NotifyComponent {
-	public DrawableComponent(ComponentSystem componentSystem) {
-		super(componentSystem);
-	}
-}
